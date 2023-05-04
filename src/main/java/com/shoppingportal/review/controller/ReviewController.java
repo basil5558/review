fe066383f37cafd8f82999c48c6a1b4fc826c28b
Sabilash-K-P <108653308+Sabilash-K-P@users.noreply.github.com>
2023-05-04 17:23:39 +0530

    Update ReviewController.java

--- src/main/java/com/shoppingportal/review/controller/ReviewController.java
+++ src/main/java/com/shoppingportal/review/controller/ReviewController.java
@@ -1,10 +1,6 @@
 package com.shoppingportal.review.controller;
 
- 
-
 import java.util.List;
-
- 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -18,78 +14,68 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
- 
-
 import com.shoppingportal.review.model.Review;
 import com.shoppingportal.review.service.ReviewService;
 
- 
 
- 
 
 
 @RestController
 public class ReviewController {
+	  
+	    private static final String DELETE_MESSAGE = "Deleted Successfully";
+	    
+		@Autowired 
+	    private ReviewService ReviewService;
+	  
+	    // Save operation
+	    @PostMapping("/Reviews")
+	    public Review saveReview(@Validated @RequestBody Review Review)
+	    {
+	        return ReviewService.saveReview(Review);
+	    }
+	  
+	    // Read operation
+	    @GetMapping("/Reviews")
+	    public List<Review> fetchReviewList(Pageable page)
+	    {
+	        return ReviewService.fetchReviewList(page);
+	    }
+	    
+	    // Read operation
+	    @GetMapping("/Reviews/{id}")
+	    public Review fetchReviewById(@PathVariable("id") long ReviewId)
+	    {
+	        return ReviewService.getReviewById(ReviewId);
+	    }
+	    
+	   // Read operation
+	    @GetMapping("/Reviews/user/{username}")
+		public List<Review> findAllReviewUsersNative(@PathVariable("username") String username) {
 
-        private static final String DELETE_MESSAGE = "Deleted Successfully";
+			return ReviewService.findAllReviewUsersNative(username);
+		}
 
-        @Autowired 
-        private ReviewService ReviewService;
+		// Read operation
+	    @GetMapping("/Reviews/product/{productId}")
+		public List<Review> findAllReviewNative(@PathVariable("productId") String productId) {
+			
+			return ReviewService.findAllReviewNative(productId);
+		}
 
-        // Save operation
-        @PostMapping("/Reviews")
-        public Review saveReview(@Validated @RequestBody Review Review)
-        {
-            return ReviewService.saveReview(Review);
-        }
-
-        // Read operation
-        @GetMapping("/Reviews")
-        public List<Review> fetchReviewList(Pageable page)
-        {
-            return ReviewService.fetchReviewList(page);
-        }
-
-        // Read operation
-        @GetMapping("/Reviews/{id}")
-        public Review fetchReviewById(@PathVariable("id") long ReviewId)
-        {
-            return ReviewService.getReviewById(ReviewId);
-        }
-
-       // Read operation
-        @GetMapping("/Reviews/user/{username}")
-        public List<Review> findAllReviewUsersNative(@PathVariable("username") String username) {
-
- 
-
-            return ReviewService.findAllReviewUsersNative(username);
-        }
-
- 
-
-        // Read operation
-        @GetMapping("/Reviews/product/{productId}")
-        public List<Review> findAllReviewNative(@PathVariable("productId") String productId) {
-
-            return ReviewService.findAllReviewNative(productId);
-        }
-
- 
-
-      
-        // Update operation
-        @PatchMapping("/Reviews/{id}")
-        public Review updateReview(@RequestBody Review Review,@PathVariable("id") long ReviewId)
-        {
-            return ReviewService.updateReview(Review, ReviewId);
-        }
-
-        // Delete operation
-        @DeleteMapping("/Reviews/{id}")
-        public String deleteReviewById(@PathVariable("id") long ReviewId)
-        {
-            ReviewService.deleteReviewById(ReviewId);
-            return DELETE_MESSAGE;
-        }
-    }
+	  
+	    // Update operation
+	    @PatchMapping("/Reviews/{id}")
+	    public Review updateReview(@RequestBody Review Review,@PathVariable("id") long ReviewId)
+	    {
+	        return ReviewService.updateReview(Review, ReviewId);
+	    }
+	  
+	    // Delete operation
+	    @DeleteMapping("/Reviews/{id}")
+	    public String deleteReviewById(@PathVariable("id") long ReviewId)
+	    {
+	        ReviewService.deleteReviewById(ReviewId);
+	        return DELETE_MESSAGE;
+	    }
+	}
